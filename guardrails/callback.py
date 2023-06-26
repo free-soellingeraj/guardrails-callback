4cc52eccdbe42122d81aab24a6385dc8760295d3
Aaron - Colab (corner-localization) <asoellin@gmail.com>
2023-06-26 18:04:00 -0400

    stripped out app specific code; shell only

--- guardrails/callback.py
+++ guardrails/callback.py
@@ -1,21 +1,14 @@
-from typing import Callable
+from typing import Callable, Any
 
 class Callback:
-    def __init__(self, before_prepare: Callable = None,
-        after_prepare: Callable = None, before_call: Callable = None):
-        self.before_prepare = before_prepare
-        self.after_prepare = after_prepare
-        self.before_call = before_call
+    def __init__(self,):
+        pass
 
     def before_prepare(self, **kwargs):
-        if self.before_prepare is not None:
-            self.before_prepare(**kwargs)
+        pass
 
     def after_prepare(self, **kwargs):
-        if self.before_prepare is not None:
-            self.after_prepare(**kwargs)
+        pass
 
     def before_call(self, **kwargs):
-        if self.before_prepare is not None:
-            self.before_call(**kwargs)
-
+        pass
